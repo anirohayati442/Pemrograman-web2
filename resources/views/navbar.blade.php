ffb91b0c4d00ba0bda813697820957553bcc7bfb
Ani Rohayati <anirohayati442@gmail.com>
2026-06-12 14:48:30 +0700

    Upload project Laravel toko_canteek

---
+++ resources/views/navbar.blade.php
@@ -0,0 +1,11 @@
+<nav class="bg-white shadow-sm mb-6">
+    <div class="w-full px-6 py-3 flex justify-between items-center">
+        <div class="flex items-center gap-8">
+            <span class="font-bold text-lg text-blue-600 tracking-wide">Toko Cantekk.Style</span>
+            <div>
+                <a href=""
+            </div>
+        </div>
+    </div>
+
+</nav>
